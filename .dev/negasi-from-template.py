ba142adce093eab719a7ce885fc2a804d119c1bb
Indra Wahyu <spot@tracert.id>
2022-10-05 03:44:43 +0800

    copas from web :)

---
+++ .dev/negasi-from-template.py
@@ -0,0 +1,14 @@
+from PIL import Image
+import matplotlib.pyplot as plt
+img=Image.open("image.jpeg")
+
+w,h=img.size
+for i in range(w):
+     for j in range(h):
+          r,g,b=img.getpixel((i,j))
+          r=255-r
+          g=255-g
+          b=255-b
+          img.putpixel((i,j),(r,g,b))
+plt.axis('off')
+plt.imshow(img) 
